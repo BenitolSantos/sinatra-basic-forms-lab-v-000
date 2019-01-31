98103a85be0aa3e1e9b5114c4e2e65cb5f94aa48
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-01-31 16:27:18 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,8 +2,14 @@ require 'pry'
 class Puppy
   attr_accessor :name, :breed, :age
   def initialize(name,breed,age)
+<<<<<<< HEAD
     @name = name
     @age = age 
+=======
+    binding.pry
+    @name = name
+    @age = age
+>>>>>>> ecd42aa17f5c9854c85a5148deca66ce08c63b7f
     @breed = breed
   end
 
